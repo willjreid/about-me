91aca18e19898820dad7431cc437f26d30cde2b0
William J Reid <willjreid@gmail.com>
2017-08-08 15:44:26 -0700

    saving properly in about-me directory

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var shirtSize = prompt('What is your shirt size? (Small/Medium/Large/Extra Large)');
+console.log('Q: What is your shirt size?');
+alert('I hope that CodeFellows has a ' + shirtSize + ' grey shirt for you!');
+console.log('A: ' + shirtSize);
