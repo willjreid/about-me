37f8b854610e65b2934a4b1b457bb6146fc507d9
William J Reid <willjreid@gmail.com>
2017-08-08 16:43:41 -0700

    troubleshooting prompt behavior

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var q1 = 'Is Will taller than you? -- Please answer "YES" or "NO" ';
-var isTaller = prompt.toUpperCase(q1);
+//var q1 = 'Is Will taller than you? -- Please answer "YES" or "NO" ';
+var isTaller = prompt('Is Will taller than you? -- Please answer "YES" or "NO" ');
 console.log('Q: ' + q1);
 if (isTaller == 'YES' || isTaller == 'Y') {
   alert('Yes, there is a 99+% chance he is taller than you.');
@@ -9,8 +9,8 @@ if (isTaller == 'YES' || isTaller == 'Y') {
 else if (isTaller == 'NO' || isTaller == 'N') {
   alert('Really? If that\'s the case, there is a spot on a local basketball team for you!');
 }
-else (isTaller != 'YES' && isTaller != 'Y' && isTaller != 'NO' && isTaller != 'N')
+/*else (isTaller != 'YES' && isTaller != 'Y' && isTaller != 'NO' && isTaller != 'N')
 {
   alert('You probably didn\'t answer the question properly--please refresh and try again.');
-}
-console.log('A: ' + isTaller);
+}*/
+//console.log('A: ' + isTaller);
