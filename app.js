2d9b63153c97aab505ad9a3d2e26f4bf537c43fb
William J Reid <willjreid@gmail.com>
2017-08-08 17:23:38 -0700

    five questions

--- app.js
+++ app.js
@@ -19,3 +19,59 @@ else if (isTaller != 'YES' && isTaller != 'NO' && isTaller != 'N' && isTaller !=
   alert('You probably didn\'t answer the question properly--please refresh and try again.');
 };
 console.log('A: ' + isTaller);
+
+var isFather = prompt(q2).toUpperCase();
+console.log('Q: ' + q2);
+if (isFather == 'YES' || isFather == 'Y') {
+  alert('Incorrect--Will is the proud father of two children.');
+}
+else if (isFather == 'NO' || isFather == 'N') {
+  alert('Right! Will is the proud father of two children.');
+}
+else if (isFather != 'YES' && isFather != 'NO' && isFather != 'N' && isFather != 'Y')
+{
+  alert('You probably didn\'t answer the question properly--please refresh and try again.');
+};
+console.log('A: ' + isFather);
+
+var hasTattoo = prompt(q3).toUpperCase();
+console.log('Q: ' + q3);
+if (hasTattoo == 'YES' || hasTattoo == 'Y') {
+  alert('Not so fast--Will is thinking about a boss wave tattoo but he hasn\'t been inked yet.');
+}
+else if (hasTattoo == 'NO' || hasTattoo == 'N') {
+  alert('Correct! Will is thinking about a boss wave tattoo but he hasn\'t been inked yet.');
+}
+else if (hasTattoo != 'YES' && hasTattoo != 'NO' && hasTattoo != 'N' && hasTattoo != 'Y')
+{
+  alert('You probably didn\'t answer the question properly--please refresh and try again.');
+};
+console.log('A: ' + hasTattoo);
+
+var lovesBurritos = prompt(q4).toUpperCase();
+console.log('Q: ' + q4);
+if (lovesBurritos == 'YES' || lovesBurritos == 'Y') {
+  alert('Of course! Will loves burritos!');
+}
+else if (lovesBurritos == 'NO' || lovesBurritos == 'N') {
+  alert('Trick question! Will loves all forms of sustinance!');
+}
+else if (lovesBurritos != 'YES' && lovesBurritos != 'NO' && lovesBurritos != 'N' && lovesBurritos != 'Y')
+{
+  alert('You probably didn\'t answer the question properly--please refresh and try again.');
+};
+console.log('A: ' + lovesBurritos);
+
+var fromSeattle = prompt(q5).toUpperCase();
+console.log('Q: ' + q5);
+if (fromSeattle == 'YES' || fromSeattle == 'Y') {
+  alert('Weeeeell... he\'s been here 5.5 years, and he no longer owns an umbrella, so he feels like a native.');
+}
+else if (fromSeattle == 'NO' || fromSeattle == 'N') {
+  alert('Will has been here 5.5 years, and he no longer owns an umbrella, so he feels like a native.');
+}
+else if (fromSeattle != 'YES' && fromSeattle != 'NO' && fromSeattle != 'N' && fromSeattle != 'Y')
+{
+  alert('You probably didn\'t answer the question properly--please refresh and try again.');
+};
+console.log('A: ' + fromSeattle);
