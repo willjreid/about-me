1f2c4c4159e60c39aa48ff9ac5700e78b2952339
Ozge Kantar Boyraz <ozge.kantar@gmail.com>
2017-08-10 16:58:56 -0700

    converting the qustions to functions

--- app.js
+++ app.js
@@ -17,137 +17,176 @@ var guestName = prompt(q0);
 console.log('Guest\'s name: ' + guestName);
 var isTaller = prompt(q1);
 console.log('Q: ' + q1);
-if (isTaller.toUpperCase() === 'YES' || isTaller.toUpperCase() === 'Y') {
-  alert('Yes, there is a 99+% chance he is taller than you.');
-  correct++;
-}
-else if (isTaller.toUpperCase() === 'NO' || isTaller.toUpperCase() === 'N') {
-  alert('Really? If that\'s the case, there is a spot on a local basketball team for you!');
-}
-else if (validAnswers.includes(isTaller.toUpperCase()) !== true)
-{
-  alert('You probably didn\'t answer the question properly--please refresh and try again.');
+function question1(isTaller){
+  if (isTaller.toUpperCase() === 'YES' || isTaller.toUpperCase() === 'Y') {
+    alert('Yes, there is a 99+% chance he is taller than you.');
+    correct++;
+  }
+  else if (isTaller.toUpperCase() === 'NO' || isTaller.toUpperCase() === 'N') {
+    alert('Really? If that\'s the case, there is a spot on a local basketball team for you!');
+  }
+  else if (validAnswers.includes(isTaller.toUpperCase()) !== true)
+  {
+    alert('You probably didn\'t answer the question properly--please refresh and try again.');
+  };
+  console.log('A: ' + isTaller);
+  return isTaller;
 };
-console.log('A: ' + isTaller);
+question1(isTaller);
 
 var isFather = prompt(q2);
 console.log('Q: ' + q2);
-if (isFather.toUpperCase() === 'YES' || isFather.toUpperCase() === 'Y') {
-  alert('Incorrect--Will is the proud father of two children.');
-}
-else if (isFather.toUpperCase() === 'NO' || isFather.toUpperCase() === 'N') {
-  alert('Right! Will is the proud father of two children.');
-  correct++;
-}
-else if (validAnswers.includes(isFather.toUpperCase()) !== true)
-{
-  alert('You probably didn\'t answer the question properly--please refresh and try again.');
+function question2(isFather){
+  if (isFather.toUpperCase() === 'YES' || isFather.toUpperCase() === 'Y') {
+    alert('Incorrect--Will is the proud father of two children.');
+  }
+  else if (isFather.toUpperCase() === 'NO' || isFather.toUpperCase() === 'N') {
+    alert('Right! Will is the proud father of two children.');
+    correct++;
+  }
+  else if (validAnswers.includes(isFather.toUpperCase()) !== true)
+  {
+    alert('You probably didn\'t answer the question properly--please refresh and try again.');
+  };
+  console.log('A: ' + isFather);
+  return isFather;
 };
-console.log('A: ' + isFather);
+question2(isFather);
 
 var hasTattoo = prompt(q3);
 console.log('Q: ' + q3);
-if (hasTattoo.toUpperCase() === 'YES' || hasTattoo.toUpperCase() === 'Y') {
-  alert('Not so fast--Will is thinking about a boss wave tattoo but he hasn\'t been inked yet.');
+function question3(hasTattoo){
+  if (hasTattoo.toUpperCase() === 'YES' || hasTattoo.toUpperCase() === 'Y') {
+    alert('Not so fast--Will is thinking about a boss wave tattoo but he hasn\'t been inked yet.');
+  }
+  else if (hasTattoo.toUpperCase() === 'NO' || hasTattoo.toUpperCase() === 'N') {
+    alert('Correct! Will is thinking about a boss wave tattoo but he hasn\'t been inked yet.');
+    correct++;
+  }
+  else if (validAnswers.includes(hasTattoo.toUpperCase()) !== true)
+  {
+    alert('You probably didn\'t answer the question properly--please refresh and try again.');
+  };
+  console.log('A: ' + hasTattoo);
+  return hasTattoo;
 }
-else if (hasTattoo.toUpperCase() === 'NO' || hasTattoo.toUpperCase() === 'N') {
-  alert('Correct! Will is thinking about a boss wave tattoo but he hasn\'t been inked yet.');
-  correct++;
-}
-else if (validAnswers.includes(hasTattoo.toUpperCase()) !== true)
-{
-  alert('You probably didn\'t answer the question properly--please refresh and try again.');
-};
-console.log('A: ' + hasTattoo);
+question3(hasTattoo);
 
 var lovesBurritos = prompt(q4);
 console.log('Q: ' + q4);
-if (lovesBurritos.toUpperCase() === 'YES' || lovesBurritos.toUpperCase() === 'Y') {
-  alert('Of course! Will loves burritos!');
-  correct++;
+function question4(lovesBurritos){
+  if (lovesBurritos.toUpperCase() === 'YES' || lovesBurritos.toUpperCase() === 'Y') {
+    alert('Of course! Will loves burritos!');
+    correct++;
+  }
+  else if (lovesBurritos.toUpperCase() === 'NO' || lovesBurritos.toUpperCase() === 'N') {
+    alert('Trick question! Will loves all forms of sustenance!');
+  }
+  else if (validAnswers.includes(lovesBurritos.toUpperCase()) !== true)
+  {
+    alert('You probably didn\'t answer the question properly--please refresh and try again.');
+  };
+  console.log('A: ' + lovesBurritos);
+  return lovesBurritos;
 }
-else if (lovesBurritos.toUpperCase() === 'NO' || lovesBurritos.toUpperCase() === 'N') {
-  alert('Trick question! Will loves all forms of sustenance!');
-}
-else if (validAnswers.includes(lovesBurritos.toUpperCase()) !== true)
-{
-  alert('You probably didn\'t answer the question properly--please refresh and try again.');
-};
-console.log('A: ' + lovesBurritos);
+question4(lovesBurritos);
 
 var fromSeattle = prompt(q5);
 console.log('Q: ' + q5);
-if (fromSeattle.toUpperCase() === 'YES' || fromSeattle.toUpperCase() === 'Y') {
-  alert('Weeeeell... he\'s been here 5.5 years, and he no longer owns an umbrella, so he feels like a native.');
+function question5(fromSeattle){
+  if (fromSeattle.toUpperCase() === 'YES' || fromSeattle.toUpperCase() === 'Y') {
+    alert('Weeeeell... he\'s been here 5.5 years, and he no longer owns an umbrella, so he feels like a native.');
+  }
+  else if (fromSeattle.toUpperCase() === 'NO' || fromSeattle.toUpperCase() === 'N') {
+    alert('Will has been here 5.5 years, and he no longer owns an umbrella, so he feels like a native.');
+    correct++;
+  }
+  else if (validAnswers.includes(fromSeattle.toUpperCase()) !== true)
+  {
+    alert('You probably didn\'t answer the question properly--please refresh and try again.');
+  };
+  console.log('A: ' + fromSeattle);
+  return fromSeattle;
 }
-else if (fromSeattle.toUpperCase() === 'NO' || fromSeattle.toUpperCase() === 'N') {
-  alert('Will has been here 5.5 years, and he no longer owns an umbrella, so he feels like a native.');
-  correct++;
-}
-else if (validAnswers.includes(fromSeattle.toUpperCase()) !== true)
-{
-  alert('You probably didn\'t answer the question properly--please refresh and try again.');
-};
-console.log('A: ' + fromSeattle);
+question5(fromSeattle);
 
 //sixth question, numbers, using a random number assignment for yearBorn
-var randYear = (Math.floor(Math.random() * 10) + 1980);
-console.log('Random Year: ' + randYear);
-
+var randYear;
+function calculate(){
+  randYear = (Math.floor(Math.random() * 10) + 1980);
+  console.log('Random Year: ' + randYear);
+  return randYear;
+}
+calculate();
 var attempts6 = 0;
 var yearBorn = prompt(q6);
-console.log('Q: ' + q5);
-while (attempts6 < 3) {
-  if (yearBorn < randYear) {
-    yearBorn = prompt('Too old, please try again.');
-    attempts6++;
-    console.log('A: ' + yearBorn);
+console.log('Q: ' + q6);
+function question6(yearBorn){
+  while (attempts6 < 3) {
+    if (yearBorn < randYear) {
+      yearBorn = prompt('Too old, please try again.');
+      attempts6++;
+      console.log('A: ' + yearBorn);
+    }
+    else if (yearBorn > randYear) {
+      yearBorn = prompt('Too young, please try again.');
+      attempts6++;
+      console.log('A: ' + yearBorn);
+    }
+    else if (isNaN(yearBorn)) {
+      yearBorn = prompt('You probably didn\'t answer the question properly--please refresh and try again.');
+      attempts6++;
+      console.log('A: ' + yearBorn);
+    }
+    else {
+      alert('Correct, Will was born in ' + yearBorn + '. (He was really born in 1982 but he has implemented a random number generator for yearBorn)');
+      correct++;
+      console.log('A: ' + yearBorn);
+      break;
+    }
+    if (attempts6 >= 3){
+      alert('Sorry Will was born in ' + randYear + '.');
+      break;
+      console.log('A: ' + yearBorn);
+    }
   }
-  else if (yearBorn > randYear) {
-    yearBorn = prompt('Too young, please try again.');
-    attempts6++;
-    console.log('A: ' + yearBorn);
-  }
-  else if (isNaN(yearBorn)) {
-    yearBorn = prompt('You probably didn\'t answer the question properly--please refresh and try again.');
-    attempts6++;
-    console.log('A: ' + yearBorn);
-  }
-  else {
-    alert('Correct, Will was born in ' + randYear + '. (He was really born in 1982 but he has implemented a random number generator for yearBorn)');
-    correct++;
-    console.log('A: ' + yearBorn);
-    break;
-  };
+  return yearBorn;
 }
-
+question6(yearBorn);
 //seventh question, numbers
 var attempts7 = 0;
 var playerLName = prompt(q7);
-console.log('Q: ' + q5);
-while (attempts7 < 5) {
-  if (validPlayers.includes(playerLName.toUpperCase()) !== true) {
-    playerLName = prompt('Nah, he sounds like a Timber. Please try again.');
-    console.log('A: ' + playerLName);
-    attempts7++;
+console.log('Q: ' + q7);
+function question7(playerLName){
+  while (attempts7 < 5) {
+    if (validPlayers.includes(playerLName.toUpperCase()) !== true) {
+      playerLName = prompt('Nah, he sounds like a Timber. Please try again.');
+      console.log('A: ' + playerLName);
+      attempts7++;
+    }
+    else {
+      alert('Correct, ' + playerLName + ' is a great hero of the Sounders!');
+      correct++;
+      console.log('A: ' + playerLName);
+      break;
+    }
   }
-  else {
-    alert('Correct, ' + playerLName + ' is a great hero of the Sounders!');
-    correct++;
-    console.log('A: ' + playerLName);
-    break;
-  };
+  return playerLName;
 }
-alert('Just FYI, Will\'s favorite players are: ' + validPlayers.toString() + ' and we can all agree the team is better off without Andy Rose.');
+question7(playerLName);
+alert('Just FYI, Will\'s favorite players are: ' + validPlayers.join(', ') + ' and we can all agree the team is better off without Andy Rose.');
 
 //tabulate the results and share with the guestName
-if (correct < 4) {
-  alert('Thanks for playing ' + guestName + '. You got ' + correct + ' questions right out of 7. Please catch up with Will over a cup of coffee, then try again.');
+function final(){
+  if (correct < 4) {
+    alert('Thanks for playing ' + guestName + '. You got ' + correct + ' questions right out of 7. Please catch up with Will over a cup of coffee, then try again.');
+  }
+  else if (4 <= correct && 7 > correct) {
+    alert('Thanks for playing ' + guestName + '. You got ' + correct + ' questions right out of 7. You are becomming quite the Will afficionado. Please try again after having a beer with Will.');
+  }
+  else if (7 === correct) {
+    alert(guestName + ', you must really know Will. You got ' + correct + ' questions right out of 7. Will should buy you a beer!');
+  }
+  console.log('Total score: ' + correct + ' out of 7.');
 }
-else if (4 <= correct && 7 > correct) {
-  alert('Thanks for playing ' + guestName + '. You got ' + correct + ' questions right out of 7. You are becomming quite the Will afficionado. Please try again after having a beer with Will.');
-}
-else if (7 === correct) {
-  alert(guestName + ', you must really know Will. You got ' + correct + ' questions right out of 7. Will should buy you a beer!');
-}
-console.log('Total score: ' + correct + ' out of 7.');
+final();
